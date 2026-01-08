3ff1846aa4a729c753d2e1540cfb3a1342b70775
Bibin Thomas <bibinthomas4897@gmail.com>
2026-01-08 13:18:31 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,16 +1,16 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD2KxHxYYrp-qKiV6LS24UCe3p-nnJvgb4",
-    authDomain: "ev-recharge-afcf0.firebaseapp.com",
-    projectId: "ev-recharge-afcf0",
-    storageBucket: "ev-recharge-afcf0.firebasestorage.app",
-    messagingSenderId: "1090414002132",
-    appId: "1:1090414002132:web:877467701f7bfc88ec6041",
-    measurementId: "G-FYCSRCYFZD"
-  };
+  apiKey: "AIzaSyD2KxHxYYrp-qKiV6LS24UCe3p-nnJvgb4",
+  authDomain: "ev-recharge-afcf0.firebaseapp.com",
+  projectId: "ev-recharge-afcf0",
+  storageBucket: "ev-recharge-afcf0.firebasestorage.app",
+  messagingSenderId: "1090414002132",
+  appId: "1:1090414002132:web:877467701f7bfc88ec6041",
+  measurementId: "G-FYCSRCYFZD"
+};
 
-   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-  // Initialize Cloud Firestore and get a reference to the service
+// Initialize Cloud Firestore and get a reference to the service
 const db = firebase.firestore();
