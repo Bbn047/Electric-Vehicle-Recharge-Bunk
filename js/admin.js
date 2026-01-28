407e4b484e4900936a9e7a875629c12bfe43ed91
Bibin Thomas <bibinthomas4897@gmail.com>
2026-01-28 03:19:12 +0100

    admin dashboard update

--- js/admin.js
+++ js/admin.js
@@ -2,25 +2,19 @@ const sidebar = document.querySelector(".left-section");
 const menuIcon = document.querySelector("#menu-icon");
 const closeIcon= document.querySelector("#close-icon");
 
-menuIcon.addEventListener("click", () =>{
-    sidebar.style.display = "flex";
-    menuIcon.style.display = "none";
-});
 
 closeIcon.addEventListener("click", () =>{
     sidebar.style.display = "none";
     menuIcon.style.display = "block";
 })
 
-function displaySize(){
-    if(window.innerWidth >=992){
-        sidebar.style.display = "flex";
-    }else{
-        sidebar.style.display= "none";
+menuIcon.addEventListener("click", () =>{
+    sidebar.style.display = "flex";
+    menuIcon.style.display = "none";
+});
+
+window.addEventListener("resize", () =>{
+    if(window.innerWidth>991){
+        sidebar.classList.remove("active");
     }
-}
-
-
-displaySize();
-
-window.addEventListener("resize", displaySize);
+});
